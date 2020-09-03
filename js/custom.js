2f617b8dd2f427ec2556437939eb51fb73eebedf
mohammed kullab <56482391+mohammedzkullab@users.noreply.github.com>
2020-09-03 15:52:18 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@ $(function () {
     $('.responsive').slick({
         dots: false,
         infinite: false,
-        autoplay: true,
+        autoplay: false,
         autoplayspeed: 2000,
         speed: 500,
         slidesToShow: 3,
@@ -15,18 +15,32 @@ $(function () {
                     slidesToShow: 3,
                     slidesToScroll: 3,
                     infinite: true,
-                    dots: true
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 500,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
@@ -42,27 +56,27 @@ $(function () {
     $('.responsive-1').slick({
         dots: false,
         infinite: false,
-        autoplay: true,
+        autoplay: false,
         autoplayspeed: 2000,
         speed: 500,
         slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         arrows: false,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: false,
+                    dots: false
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             },
             {
@@ -78,5 +92,37 @@ $(function () {
         ]
     });
 });
+$('#navbar a, .btn ,#footer a').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
 
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top,
+            },
+            800
+        );
+    }
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
 
